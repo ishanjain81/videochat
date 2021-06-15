7ff154da9f32ef6e73698e2361182b107a500304
Ishan Jain <ishanjaincool81@gmail.com>
2021-06-15 08:47:40 +0530

    Initialize uuidv4

--- config/sockets.js
+++ config/sockets.js
@@ -1,4 +1,5 @@
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer);
-    
+    const { v4: uuidv4 } = require('uuid');
+    console.log(`${uuidv4()}`);
 }
