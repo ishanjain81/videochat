be709cabe8deec8c9c274ca76bd7a4ffd5e774ae
Ishan Jain <ishanjaincool81@gmail.com>
2021-06-24 15:12:35 +0530

    Setting Up User Schema

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 //render sign up page
 module.exports.signUp = function(req,res){
     return res.render('user_sign_up',{
@@ -10,4 +12,23 @@ module.exports.signIn = function(req,res){
     return res.render('user_sign_in',{
         title: "Teams | Sign In"
     })
+}
+
+//creating a User in DB
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email},function(err, user){
+        if(err){console.log("Error",err); return}
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){console.log('Error in creating a User',err); return}
+                return res.redirect('/users/sign-in');
+            });
+        }
+        else{
+            return res.redirect('/users/sign-in');
+        }
+    });
 }
