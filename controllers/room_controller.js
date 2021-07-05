3c0391861e1eba4ab2b440d80ef700ff0c1a5f9e
Ishan Jain <ishanjaincool81@gmail.com>
2021-07-05 17:10:07 +0530

    Handling error in Room

--- controllers/room_controller.js
+++ controllers/room_controller.js
@@ -1,5 +1,9 @@
 const invite = require('../mailers/invite-mailer');
 module.exports.room_render = function(req,res){
+    if(!req.isAuthenticated()){
+        req.flash('success','Please ! Login to Continue');
+        return res.redirect('/users/sign-in');
+    }
     return res.render('room',{
         title : "Room",
         roomId : req.params.room_id
